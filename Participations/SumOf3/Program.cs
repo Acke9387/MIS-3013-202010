6763f96ac0df326ce385f900d754e23593a3a9a8
Professor Ackerman <adam@ou.edu>
2020-09-01 12:24:38 -0500

    Started the sumof3 participation

---
+++ Participations/SumOf3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SumOf3
+{
+    class Program
+    {
+        const double LUCKY_NUMBER = 7.777;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
