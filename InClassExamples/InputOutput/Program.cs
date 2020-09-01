6883f585c7cb3a5dc22ef85e7984d3c0d5ccb051
Professor Ackerman <adam@ou.edu>
2020-09-01 12:13:36 -0500

    Finished the input/output example for in class

--- InClassExamples/InputOutput/Program.cs
+++ InClassExamples/InputOutput/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿/*
+ * 
+ * @Author: Professor Ackerman
+ * Collaborated with : The class
+ */
+using System;
+using System.Threading;
 
 namespace InputOutput
 {
     class Program
     {
+        const string CLASS_NAME = "MIS 3013";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //This won't work because it is a constant
+            //  CLASS_NAME = "MIS 3353";
+            //string firstName, lastName;
+            string firstName = "Adam";
+            string lastName = "Ackerman";
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine(); //Getting what the user typed and storing it in the variable firstName
+
+            Console.WriteLine("Please enter your last name >> ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your GPA? >>");
+            string gpaInput = Console.ReadLine();
+            double gpa = Convert.ToDouble(gpaInput);
+
+            //This doesn't make sense, BUT it shows we can use it in calculations
+            double gpaEnhanced = gpa + 1.5;
+
+            Console.WriteLine(firstName + " " + lastName + 
+                " is teaching " + CLASS_NAME  +
+                " with a GPA of " + gpa.ToString("N2"));
         }
     }
 }
