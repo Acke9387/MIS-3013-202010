eeae42b196a53743f43a36ac2a850f91b73e40e8
Professor Ackerman <adam@ou.edu>
2020-09-01 11:49:37 -0500

    Created new repository with starter solutions + projects

---
+++ Participations/Participation1_SumOf3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation1_SumOf3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
