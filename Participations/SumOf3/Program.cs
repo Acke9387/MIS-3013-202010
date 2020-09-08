152f985cfb80459d88bcf251ed8c1ad71c196407
Professor Ackerman <adam@ou.edu>
2020-09-08 11:37:59 -0500

    Finished the code for sum of 3

--- Participations/SumOf3/Program.cs
+++ Participations/SumOf3/Program.cs
@@ -7,7 +7,30 @@ namespace SumOf3
         const double LUCKY_NUMBER = 7.777;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double num1, num2, num3, sum;
+            
+            //double val1;
+            //double val2;
+            //double val3;
+            string message3 = "Please enter your third number >>";
+
+            Console.WriteLine("Please enter your first number >>");
+            string input = Console.ReadLine();
+            num1 = Convert.ToDouble(input);
+
+            Console.WriteLine("Please enter your second number >>");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(message3);
+            num3 = Convert.ToDouble(Console.ReadLine());
+
+            sum = num1 + num2 + num3;
+            Console.WriteLine($"The sum of all 3 numbers is {sum.ToString("N3")}.");
+
+            double luckyNum;
+            luckyNum = sum * LUCKY_NUMBER;
+
+            Console.WriteLine($"Your lucky # is {luckyNum.ToString("N3")}.");
 
         }
     }
