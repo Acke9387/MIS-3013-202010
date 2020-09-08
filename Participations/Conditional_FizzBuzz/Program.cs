b1537a7832d0a395a40d967a62f414b0c775b77a
Professor Ackerman <adam@ou.edu>
2020-09-08 11:57:50 -0500

    Started the Conditional FizzBuzz problem

---
+++ Participations/Conditional_FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Conditional_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            //int remainder = number % 3;
+            //bool hasRemainder = remainder == 0;
+
+            //BOOLEAN OPERATORS:
+            //      OR  = ||
+            //      AND = &&
+
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
