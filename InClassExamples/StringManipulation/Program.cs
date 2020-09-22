0fef2ba46f1b714d24d0e1bc2bfaea12c97b6d42
Professor Ackerman <adam@ou.edu>
2020-09-22 12:13:25 -0500

    Added in clas example over string manipulation

---
+++ InClassExamples/StringManipulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                         012345678910
+            string instanceOfString = "Hello World!";
+
+            instanceOfString = instanceOfString.ToUpper();
+            for (int i = 0; i < instanceOfString.Length; i++)
+            {
+                char letter = instanceOfString[i];
+                // Have to conver the CHARACTER to a string to use the String methods
+                Console.WriteLine(letter);//.ToString().ToUpper());
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
