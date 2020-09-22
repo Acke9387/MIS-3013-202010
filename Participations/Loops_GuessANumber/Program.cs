c9105423bf29f179e67b74a6859ee169d20b7450
Professor Ackerman <adam@ou.edu>
2020-09-22 11:50:48 -0500

    Completed the guess a number loop participation and extra work on it

---
+++ Participations/Loops_GuessANumber/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Loops_GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an INSTANCE of Random
+            // or create an OBJECT of type Random
+            // so we can eventually generate a random number
+            Random rand = new Random();
+
+            //Console.WriteLine("Please enter the minimum value for the random number >>");
+            string minimumString;// = Console.ReadLine();
+            //int minimum = Convert.ToInt32(minimumString);
+            int minimum, maximum, guess;
+            bool success;
+
+
+            do
+            {
+                Console.WriteLine("Please enter the minimum value for the random number >>");
+                minimumString = Console.ReadLine();
+                success = Int32.TryParse(minimumString, out minimum);
+            } while (success == false);
+
+            do
+            {
+                Console.WriteLine("Please enter the maximum value for the random number >>");
+                //success = Int32.TryParse(minimumString, out maximum);
+            } while (Int32.TryParse(Console.ReadLine(), out maximum) == false);
+
+            // Create a random integer between minimum and maximum + 1
+            // Since the upper bound is EXCLUSIVE
+            int randomNumber = rand.Next(minimum, maximum + 1);
+
+            Console.WriteLine(randomNumber);
+            //int guess;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine($"Guess the random number between {minimum} - {maximum} >>");
+
+                } while (Int32.TryParse(Console.ReadLine(), out guess) == false);
+
+            } while (guess != randomNumber);
+
+            Console.WriteLine("Congratulations you got it right!");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/Loops_RandomGuess/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Loops_RandomGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
