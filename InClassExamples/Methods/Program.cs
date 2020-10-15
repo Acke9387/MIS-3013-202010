fc5a9019e960db2880170e13ef8f306d9d427be3
Professor Ackerman <adam@ou.edu>
2020-10-15 12:18:20 -0500

    Added in class example going over methods and do while and while and lists

---
+++ InClassExamples/Methods/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Methods
+{
+    class Program
+    {
+        static List<int> values = new List<int>();
+
+        static void Main(string[] args)
+        {
+
+            // Random rand = new Random(); // this is an instance
+
+            //bool isSuccess = PossiblyAddStringToAList("cat");
+            //if(isSuccess == false)
+            //{
+            //    Console.WriteLine("Cat wasn't a valid integer :(");
+            //}
+
+            //isSuccess = PossiblyAddStringToAList("5");
+            string answer = "";
+            OutputAllValuesFromList();
+
+            values.AddRange(new List<int> { 5, 10, 11, 13, 17, 28, 48, 91 });
+
+            OutputAllValuesFromList();
+            do
+            {
+                Console.WriteLine("Please enter a integer >>");
+                string intValue = Console.ReadLine();
+                bool shouldIAskAgain = PossiblyAddStringToAList(intValue);
+
+                while (shouldIAskAgain == false)
+                {
+                    Console.WriteLine("Enter a valid integer this time >>");
+                    intValue = Console.ReadLine();
+                    shouldIAskAgain = PossiblyAddStringToAList(intValue);
+                }
+
+                Console.WriteLine("Do you want to enter another number? >>");
+                answer = Console.ReadLine();
+            } while (answer == "yes");
+
+            OutputAllValuesFromList();
+            OutputEvenNumbersFromList();
+            OutputOddNumbersFromList();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Checks to make sure that a string is a valid Integer and if it is, will add it to the List
+        /// </summary>
+        /// <param name="inputFromWhoeverCalledMe">A string that holds an integer value (hopefully)</param>
+        /// <returns>
+        /// True if it was able to convert the value and add it to the list
+        /// False if it was unable to convert the value
+        /// </returns>
+        static bool PossiblyAddStringToAList(string inputFromWhoeverCalledMe)
+        {
+            bool wasSuccess = true;
+            int value;
+
+            wasSuccess = int.TryParse(inputFromWhoeverCalledMe, out value);
+
+            if (wasSuccess)
+            {
+                // we want to add it to the list
+                values.Add(value);
+            }
+            else
+            {
+                // we want to tell them they had an invalid input
+                Console.WriteLine($"{inputFromWhoeverCalledMe} was not a valid integer.  You must enter an integer");
+            }
+
+            return wasSuccess;
+        }
+
+        static void OutputAllValuesFromList()
+        {
+            Console.WriteLine("\nAll Values!!!\n");
+            for (int i = 0; i < values.Count; i++)
+            {
+                int currentValueInList = values[i];
+
+                Console.WriteLine(currentValueInList.ToString("N0"));
+            }
+        }
+
+        static void OutputOddNumbersFromList()
+        {
+            Console.WriteLine("\nAll Odd Values!!!\n");
+            for (int i = 0; i < values.Count; i++)
+            {
+                int currentValueInList = values[i];
+
+                if (currentValueInList % 2 != 0)
+                {
+                    Console.WriteLine(currentValueInList.ToString("N0"));
+                }
+            }
+        }
+        static void OutputEvenNumbersFromList()
+        {
+            Console.WriteLine("\nAll Even Values!!!\n");
+            for (int i = 0; i < values.Count; i++)
+            {
+                int currentValueInList = values[i];
+
+                if (currentValueInList % 2 == 0)
+                {
+                    Console.WriteLine(currentValueInList.ToString("N0"));
+                }
+            }
+        }
+    }
+}
--- Participations/ProcessingACSVFile/Program.cs
+++ Participations/ProcessingACSVFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ProcessingACSVFile
@@ -23,14 +24,35 @@ namespace ProcessingACSVFile
             double sumFor2003 = 0;
             double sumFor2004 = 0;
             double sumFor2005 = 0;
+            double jan = 0;
+            double feb = 0;
+            double mar = 0;
+            double apr = 0;
+            double may = 0;
+            double jun = 0;
+            double jul = 0;
+            double aug = 0;
+            double sep = 0;
+            double oct = 0;
+            double nov = 0;
+            double dec = 0;
+
+            //Dictionary<string, double> monthSums = new Dictionary<string, double>();
+            List<double> monthValues = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 13 0's even tho there are 12 months.  This means index 0 is not for any month
 
             for (int i = 1; i < lines.Length; i++)
             {
                 string line = lines[i];
-                string[] pieces = line.Split(',');
+                // Looks like this:
+                //10107,30,95.7,2,2871,2/24/2003 0:00,Shipped,1,2,2003,Motorcycles,95,S10_1678,Land of Toys Inc.,2125557818,897 Long Airport Avenue,,NYC,NY,10022,USA,NA,Yu,Kwai,Small
+                string[] pieces = line.Split(",");
                 double sale = Convert.ToDouble(pieces[4]);
                 string year = pieces[9];
                 //sum = sum + Convert.ToDouble(pieces[4]);
+                string month = pieces[8];
+
+                monthValues[Convert.ToInt32(month)] += sale;
+
                 if (pieces[6].Trim().ToLower() == "shipped")
                 {
                     if (year == "2003")
@@ -46,6 +68,61 @@ namespace ProcessingACSVFile
                         sumFor2005 += sale;
                     }
 
+
+
+                    //  Dictionary stuff below
+                    //if (monthSums.ContainsKey(month))
+                    //{
+                    //    monthSums[month] += sale;
+                    //}
+                    //else
+                    //{
+                    //    monthSums.Add(month, sale);
+                    //}
+
+                    //switch (month)
+                    //{
+                    //    case "1":
+                    //        jan += sale;
+                    //        break;
+                    //    case "2":
+                    //        feb += sale;
+                    //        break;
+                    //    case "3":
+                    //        mar += sale;
+                    //        break;
+                    //    case "4":
+                    //        apr += sale;
+                    //        break;
+                    //    case "5":
+                    //        may += sale;
+                    //        break;
+                    //    case "6":
+                    //        jun += sale;
+                    //        break;
+                    //    case "7":
+                    //        jul += sale;
+                    //        break;
+                    //    case "8":
+                    //        aug += sale;
+                    //        break;
+                    //    case "9":
+                    //        sep += sale;
+                    //        break;
+                    //    case "10":
+                    //        oct += sale;
+                    //        break;
+                    //    case "11":
+                    //        nov += sale;
+                    //        break;
+                    //    case "12":
+                    //        dec += sale;
+                    //        break;
+                    //    default:
+                    //        break;
+                    //}
+
+
                     sum += sale; 
                 }
 
@@ -55,9 +132,78 @@ namespace ProcessingACSVFile
             Console.WriteLine($"The total sales for shipped items in 2004 is {sumFor2004.ToString("C")}!");
             Console.WriteLine($"The total sales for shipped items in 2005 is {sumFor2005.ToString("C")}!");
 
+            for (int i = 1; i < monthValues.Count; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine($"The total sales for shipped items in January is {monthValues[i].ToString("C")}");
+                        break;
+                    case 2:
+
+                    default:
+                        break;
+                }
+            }
+
+            //Dictionary stuff below
+            //foreach (var month in monthSums.Keys)
+            //{
+            //    switch (month)
+            //    {
+            //        case "1":
+            //            Console.WriteLine($"The total sales for January is {monthSums[month].ToString("C")}");
+            //            break;
+            //        case "2":
+            //            feb += sale;
+            //            break;
+            //        case "3":
+            //            mar += sale;
+            //            break;
+            //        case "4":
+            //            apr += sale;
+            //            break;
+            //        case "5":
+            //            may += sale;
+            //            break;
+            //        case "6":
+            //            jun += sale;
+            //            break;
+            //        case "7":
+            //            jul += sale;
+            //            break;
+            //        case "8":
+            //            aug += sale;
+            //            break;
+            //        case "9":
+            //            sep += sale;
+            //            break;
+            //        case "10":
+            //            oct += sale;
+            //            break;
+            //        case "11":
+            //            nov += sale;
+            //            break;
+            //        case "12":
+            //            dec += sale;
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
+
+            Console.WriteLine($"The total sales for shipped items in January is {jan.ToString("C")}!");
+            Console.WriteLine($"The total sales for shipped items in February is {feb.ToString("C")}!");
+            Console.WriteLine($"The total sales for shipped items in January is {jan.ToString("C")}!");
+            Console.WriteLine($"The total sales for shipped items in January is {jan.ToString("C")}!");
+            Console.WriteLine($"The total sales for shipped items in January is {jan.ToString("C")}!");
+            Console.WriteLine($"The total sales for shipped items in January is {jan.ToString("C")}!");
+            Console.WriteLine($"The total sales for shipped items in January is {jan.ToString("C")}!");
+
             Console.WriteLine($"Total sales of shipped orders is {sum.ToString("C2")}.");
 
             Console.ReadKey();
         }
+
     }
 }
