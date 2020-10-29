31385a1862515dc54bb8ca83dc676917f20c402c
Professor Ackerman <adam@ou.edu>
2020-10-29 12:31:23 -0500

    Did in class example over classes dealing with the Rectangle

---
+++ InClassExamples/Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(10, 15);
+            double r1Area = r1.Area();
+            Console.WriteLine($"The area of r1 is {r1Area.ToString("n1")}");
+            Rectangle r2 = new Rectangle(7, 8);
+            Console.WriteLine($"The area of r2 is {r2.Area().ToString("n1")}");
+            
+
+            Console.WriteLine($"The area of this rectangle is {new Rectangle(5,5).Area()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InClassExamples/Classes/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+        /// <summary>
+        /// This is the default/empty constructor for the class.
+        /// </summary>
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+        }
+
+        /// <summary>
+        /// This is the overloaded constructor passing in the starting values for width and height
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Area()
+        {
+            double result = width * height;
+
+            return result;
+        }
+    }
+}
