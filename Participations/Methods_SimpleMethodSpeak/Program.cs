d2f6153669cae71f2c426d4239ddf38b34b072b8
Professor Ackerman <adam@ou.edu>
2020-10-29 11:34:33 -0500

    Completed the participation for the Method participation

---
+++ Participations/Methods_SimpleMethodSpeak/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Threading;
+
+namespace Methods_SimpleMethodSpeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a type of animal >>");
+            string animal = Console.ReadLine();
+            string sound = Speak(animal);
+            Console.WriteLine(sound);
+
+            Console.WriteLine(Speak("goat")); // chaining method calls
+
+            Console.ReadKey();
+        }
+
+        static string Speak(string animalType)
+        {
+            string output;// = "";
+
+            //switch (animalType.ToLower())
+            //{
+            //    case "dog":
+            //        output = "ruff ruff";
+            //        break;
+            //    case "monkey":
+            //        output = "oooo ahhh";
+            //        break;
+            //    case "goat":
+            //        output = "<person yelling>";
+            //        break;
+            //    default:
+            //        output = "Sorry I do not recognize the animal of " + animalType + ".";
+            //        break;
+            //}
+
+            if (animalType.ToLower() == "dog")
+            {
+                output = "ruff ruff";
+            }
+            else if (animalType.ToLower() == "monkey")
+            {
+                output = "oooo ahhh";
+            }
+            else if (animalType.ToLower() == "goat")
+            {
+                output = "<person yelling>";
+            }
+            else
+            {
+                output = "Sorry I do not recognize the animal of " + animalType + ".";
+            }
+
+            return output;
+
+        }
+
+    }
+}
