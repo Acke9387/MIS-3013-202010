ecd1d45e661dc1c6c80e6e6e97da11d84f5d602a
Professor Ackerman <adam@ou.edu>
2020-11-05 12:11:41 -0600

    Added an example of classes vs parallal collections

---
+++ InClassExamples/ClassesVsParallelCollections/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesVsParallelCollections
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string FavoriteFood { get; set; }
+
+        public double AccountBalance { get; set; }
+
+        //public void SetAccountBalance (double val)
+        //{
+        //    AccountBalance = val;
+        //}
+
+        //public Double GetAccountBalance()
+        //{
+        //    return AccountBalance;
+        //}
+
+        public Person()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            DateOfBirth = string.Empty;
+            FavoriteFood = string.Empty;
+            AccountBalance = 0;
+        }
+
+        public Person (string fName, string lName, string dob, string food, double bal)
+        {
+            FirstName = fName;
+            LastName = lName;
+            DateOfBirth = dob;
+            FavoriteFood = food;
+            AccountBalance = bal;
+        }
+
+        public override string ToString()
+        {
+            string output = $"{FirstName} {LastName} {DateOfBirth} likes {FavoriteFood} and has {AccountBalance.ToString("C")}";
+            return output;
+        }
+    }
+}
---
+++ InClassExamples/ClassesVsParallelCollections/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClassesVsParallelCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstNames = new List<string>();
+            List<string> lastNames = new List<string>();
+            List<string> dobs = new List<string>();
+            List<string> favoriteFood = new List<string>();
+            List<double> accountBalances = new List<double>();
+
+            firstNames.Add("Adam");
+            lastNames.Add("Ackerman");
+            dobs.Add("11/5/2020");
+            favoriteFood.Add("Pizza");
+            accountBalances.Add(500);
+
+            firstNames.Add("Micah");
+            lastNames.Add("Tison");
+            dobs.Add("6/5/2020");
+            favoriteFood.Add("French Cuisine");
+            accountBalances.Add(55500);
+
+            for (int i = 0; i < firstNames.Count; i++)
+            {
+                Console.WriteLine($"{firstNames[i]} {lastNames[i]} {dobs[i]} likes {favoriteFood[i]} and has {accountBalances[i].ToString("C")}. ");
+            }
+
+            // SAME BUT USE CLASSES NOW !!!!!
+            Person adam = new Person();
+            adam.FirstName = "Adam";
+            adam.LastName = "Ackerman";
+            adam.DateOfBirth = "11/5/2020";
+            adam.FavoriteFood = "Pizza";
+            adam.AccountBalance = 500;
+            //adam.SetAccountBalance(500);
+
+            Person micah = new Person("Micah", "Tison", 
+                                      "6/4/2020", "French Cuisine", 
+                                      55000);
+            Console.WriteLine(adam.ToString());
+            Console.WriteLine(micah);
+
+            List<Person> peeps = new List<Person>();
+            peeps.Add(micah);
+            peeps.Add(adam);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
