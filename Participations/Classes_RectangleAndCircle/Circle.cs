120fcd245e620d613b9cdd01cfadd21bf135e202
Professor Ackerman <adam@ou.edu>
2020-11-10 11:57:05 -0600

    Started the participation over classes dealing with rectangles and circles.  completed the Circle class and created some instances of the class

---
+++ Participations/Classes_RectangleAndCircle/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_RectangleAndCircle
+{
+    class Circle
+    {
+
+        public double Radius { get; set; }
+        
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+    }
+}
---
+++ Participations/Classes_RectangleAndCircle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes_RectangleAndCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circle myCircle = new Circle();
+            myCircle.Radius = 7.5;
+            Console.WriteLine($"The area of a circle with {myCircle.Radius} is {myCircle.CalculateArea()}\n" +
+                              $"   and a perimeter of {myCircle.CalculatePerimeter()}");
+
+            Circle circle2 = new Circle();
+            circle2.Radius = 8282828282;
+            double areaOfCircle2 = circle2.CalculateArea();
+            double perimeterOfCircle2 = circle2.CalculatePerimeter();
+
+            Circle circle3 = new Circle(3.5);
+
+            Console.ReadKey();
+        }
+    }
+}
