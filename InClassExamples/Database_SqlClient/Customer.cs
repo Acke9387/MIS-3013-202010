6e5bc86cd51950c34ffc8724292ef9e169d37443
Ackerman, Adam P <adamackrmn@gmail.com>
2020-12-01 12:08:53 -0600

    Completed the in class example going over connecting to a SQL database

---
+++ InClassExamples/Database_SqlClient/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_SqlClient
+{
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public Customer()
+        {
+            CustomerID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Gender = string.Empty;
+            Address = string.Empty;
+            City = string.Empty;
+            State = string.Empty;
+            ZipCode = string.Empty;
+        }
+
+        public Customer(int customerId, string firstName, string lastName, string email, string gender, string address, string city, string state, string zipCode)
+        {
+            CustomerID = customerId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Gender = gender;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} ({CustomerID}) - {Email} {Gender} \n{Address} \n{City}, {State} {ZipCode}\n";
+        }
+    }
+}
---
+++ InClassExamples/Database_SqlClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_SqlClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=tcp:s13.winhost.com;Initial Catalog=DB_128040_practice;User ID=student;Password=AdamTheGreat2020;Integrated Security=False;";
+            string queryString = " SELECT CustomerID, FirstName, LastName, Email, Gender, Address, City, State, ZipCode " +
+                                 " FROM   CUSTOMER ";
+
+            List<Customer> customers = new List<Customer>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int customerId = reader.GetInt32(0); // this is at position 0 because we listed it first in our SELECT statement
+                        string firstName = reader.GetString(1);
+                        string lastName = reader.GetString(2);
+                        string email = reader.GetString(3);
+                        string gender = reader.GetString(4);
+                        string address = reader.GetString(5);
+                        string city = reader.GetString(6);
+                        string state = reader.GetString(7);
+                        string zipCode = reader.GetString(8);
+
+                        Customer newCustomer = new Customer(customerId, firstName, lastName, email, gender, address, city, state, zipCode);
+                        customers.Add(newCustomer);
+                        //Console.WriteLine($"{firstName} {lastName} ({customerId}) - {email} {gender} \n{address} \n{city}, {state} {zipCode}\n");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("All of our customers");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine("All customers from the state Oklahoma we have customers: ");
+            foreach (var customer in customers)
+            {
+                if (customer.State.ToLower() == "oklahoma")
+                {
+                    Console.WriteLine(customer); 
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
