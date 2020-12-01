8125170c8497e443141a0c1a53bc6ca0695bf6fd
Ackerman, Adam P <adamackrmn@gmail.com>
2020-12-01 11:35:35 -0600

    Completed the TextBox participation

---
+++ Participations/Classes_TextBox/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes_TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox txt1 = new TextBox();
+            txt1.BackColor = ConsoleColor.Green;
+            txt1.ForeColor = ConsoleColor.Magenta;
+            txt1.Text = "Hello World!";
+            txt1.Padding = 5;
+
+            TextBox txt2 = new TextBox();
+            txt2.BackColor = ConsoleColor.Magenta;
+            txt2.ForeColor = ConsoleColor.Green;
+            txt2.Text = "Goodbye!";
+            txt2.Padding = 1;
+
+            txt1.DisplayText();
+            txt2.DisplayText();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/Classes_TextBox/TextBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+            Padding = 5; //Default should be 5 because there should always be 5 for padding unless otherwise
+            BackColor = ConsoleColor.Black;
+            ForeColor = ConsoleColor.White;
+            Text = string.Empty;
+        }
+
+        public void DisplayText()
+        {
+            string output = "";
+            string padding = "";
+            // First way to get the padding
+            for (int i = 0; i < Padding; i++)
+            {
+                padding += " ";
+            }
+            output = $"{padding}{Text}{padding}";
+
+            // Second way to get the padding
+            output = Text.PadLeft(Text.Length + Padding);
+            output = output.PadRight(output.Length + Padding);
+
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            Console.WriteLine(output);
+        }
+
+    }
+}
